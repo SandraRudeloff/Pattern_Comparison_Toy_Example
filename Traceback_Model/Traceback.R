a3fab10d0066f0b0db7e9d4d78e05bfa1a8f7137
Sandra Rudeloff <srudeloff@outlook.com>
2023-06-10 22:34:12 +0200

    optimized script

--- Traceback_Model/Traceback.R
+++ Traceback_Model/Traceback.R
@@ -1,68 +1,31 @@
----
-output:
-  pdf_document: default
-  html_document: default
----
-# Libraries
-
-```{r warning=TRUE}
 library(spatstat) 
 library(reticulate) 
 library(RColorBrewer) 
 pd <- import("pandas")
 library(readxl)
-```
 
 # Definition of input Data
-
-```{r}
 chains_to_investigate <- list("Chain 1" )
-```
-
-```{r}
 investigation_scenario <- 3
-```
 
 # Read Data
-
-## Window
-
-```{r}
 window <- owin(c(0,1000), c(0,1000))
-```
 
-## Outbreak
-
-Outbreak made with Diffusion Model:
-
-```{r}
-#outbreak_data <- pd$read_pickle(paste("C:\\Users\\srude\\Documents\\Dev Kram\\Toy Example Traceback Model\\Diffusion Model\\Outputs\\Outbreaks\\Scenario_", as.character(scenario), "\\Outbreak_Chain 1_10_0.pkl", sep = ""))
+## Outbreak made with Diffusion Model:
+# outbreak_data <- pd$read_pickle(paste("C:\\Users\\srude\\Documents\\Dev Kram\\Toy Example Traceback Model\\Diffusion Model\\Outputs\\Outbreaks\\Scenario_", as.character(scenario), "\\Outbreak_Chain 1_10_0.pkl", sep = ""))
 # outbreak_data <- pd$read_pickle("./Data/Outbreaks/Toy_Outbreak_10.pkl") 
 # ppp_outbreak <- ppp(x=outbreak_data$x_centroid, y=outbreak_data$y_centroid, window = window)
-```
 
-Outbreak artificially made to test model:
-```{r}
+# Outbreak artificially made to test model:
 df_outbreak <- subset(read_excel("./Data/scenarios.xlsx", sheet = "Outbreak_Locations"), scenario == investigation_scenario)
-```
-
-
-
-```{r}
 ppp_outbreak <- ppp(x = df_outbreak$case_x, y = df_outbreak$case_y, window = window ) 
 ppp_outbreak <- rescale(ppp_outbreak, 1000, "km")
-```
+
 
 ## Population Data
-
-
-```{r}
 no_of_cells <- 100 
 population_per_cell <- subset(read_excel("./Data/scenarios.xlsx", sheet = "Population"), scenario == investigation_scenario)$population_per_cell
-```
 
-
-```{r}
 # calculate number of cells per row
 cells_per_row <- sqrt(no_of_cells)
 
@@ -74,62 +37,45 @@ df_population <- expand.grid(x_centroid = centroid_coords, y_centroid = centroid
 
 # assign population
 df_population$population <- rep(population_per_cell, nrow(df_population))
-```
 
-```{r}
 ppp_population <- ppp(x = df_population$x_centroid, y = df_population$y_centroid, window = window, marks = df_population$population)
 ppp_population <- rescale(ppp_population, 1000, "km")
 smo_population <- density(ppp_population, eps = 0.1, positive = TRUE, weights = marks(ppp_population)) 
-```
 
-```{r}
 im_population <- smo_population
 im_population <- eval.im(im_population / 100)
 im_population <- eval.im(pmax(im_population, 1e-10))
-```
 
-
-```{r}
 plot(im_population)
-```
 
-# Shops Data
-Private Computer
-```{r eval=FALSE}
-shops <- pd$read_pickle(paste("C:\\Users\\srude\\Documents\\Pattern Comparison Project\\Toy_Example\\Diffusion_Model\\Outputs\\Stores\\stores_", as.character(scenario), ".pkl", sep = ""))
-```
-Work Computer
-```{r eval=FALSE}
-shops <- pd$read_pickle(paste("C:\\Users\\Sandra.Rudeloff\\Documents\\Pattern Comparison Project\\Toy_Example\\Diffusion_Model\\Outputs\\Stores\\stores_", as.character(scenario), ".pkl", sep = ""))
-```
 
-```{r}
+## Private Computer
+## shops <- pd$read_pickle(paste("C:\\Users\\srude\\Documents\\Pattern Comparison Project\\Toy_Example\\Diffusion_Model\\Outputs\\Stores\\stores_", as.character(scenario), ".pkl", sep = ""))
+
+
+## Work Computer
+## shops <- pd$read_pickle(paste("C:\\Users\\Sandra.Rudeloff\\Documents\\Pattern Comparison Project\\Toy_Example\\Diffusion_Model\\Outputs\\Stores\\stores_", as.character(scenario), ".pkl", sep = ""))
+
+
+## Shops Data
 df_shops <- subset(read_excel("./Data/scenarios.xlsx", sheet = "Store_Locations"), scenario == investigation_scenario)
-```
 
 
-```{r}
+## --------------------------------------------------------------------------
 ppp_shops <- ppp(x = df_shops$store_x, y = df_shops$store_y, window = window, marks = as.factor(df_shops$chain))
 ppp_shops <- rescale(ppp_shops, 1000, "km")
-```
-# Quadrature Scheme
 
-```{r}
+
+## Quadrature Scheme
 Q <- quadscheme(ppp_outbreak, eps = 0.1)
-```
 
-# Null Model
 
-```{r}
+## Null Model
 fit0 <- ppm(Q ~ offset(log(im_population)))
 print(fit0)
-```
-
-$$\lambda_0(u) = e^{-3.912}Z(u) = 0.02 Z(u)$$
 
 
-# Alternative Model
-```{r}
+## Alternative Model
 create_raisin_func <- function(x, y) {
   # Construct raisin expressions
   ls_all_raisins <- mapply(function(xi, yi) {
@@ -144,10 +90,8 @@ create_raisin_func <- function(x, y) {
 
   return(raisin_func)
 }
-```
 
 
-```{r}
 fit_model <- function(Q, im_population, fit0, chain) {
   fit1 <- ippm(Q ~ offset(log(im_population) + raisin_func),
                start = list(alpha = 5, beta = 15), nlm.args = list(stepmax = 1), gcontrol = glm.control(maxit = 1000))
@@ -155,18 +99,16 @@ fit_model <- function(Q, im_population, fit0, chain) {
   print(fit1)
   return(fit1)
 }
-```
 
-```{r}
+
 calculate_anova <- function(fit0, fit1, chain) {
   print(sprintf("Anova for %s", chain))
   anova_result <- (anova(fit0, fit1, test = "LRT"))
   print(anova_result)
   return(anova_result)
 }
-```
 
-```{r}
+
 plot_example <- function(ppp_chosen) {
   X <- layered(im_population, unmark(subset(ppp_shops, marks != chain, drop = TRUE)), ppp_chosen, ppp_outbreak)
   layerplotargs(X)[[1]] <- list(col = brewer.pal(n = 8, name = "Greys"), breaks = c(0, 1, 10, 50, 100, 150, 200, 250, 260))
@@ -175,10 +117,8 @@ plot_example <- function(ppp_chosen) {
   layerplotargs(X)[[4]] <- list(pch = 20, col = "red2", cex = 1.5)
   return(X)
 }
-```
 
 
-```{r}
 for (chain in chains_to_investigate) {
   print(chain)
   ppp_chosen <- unmark(subset(ppp_shops, marks == chain, drop = TRUE))
@@ -190,6 +130,5 @@ for (chain in chains_to_investigate) {
   fit1 <- fit_model(Q, im_population, fit0, chain)
   anova_result <- calculate_anova(fit0, fit1, chain)
 }
-```
 
-
+gc()
