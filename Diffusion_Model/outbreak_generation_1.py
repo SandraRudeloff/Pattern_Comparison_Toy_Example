803b8754d815d425515d15f18e399bf3cb61423e
Sandi-hub <you@example.com>
2023-09-08 15:15:04 +0200

    NOT A WORKING VERSION - tried implementation w/ outbreak generation

--- Diffusion_Model/outbreak_generation_1.py
+++ Diffusion_Model/outbreak_generation_1.py
@@ -5,28 +5,11 @@ import pandas as pd
 # from gravity_model import *
 from monte_carlo_simulation import generate_outbreak
 
-##### Definition of input Data #####
-scenario = 1
 
-## Outbreak sizes ##
-list_outbreak_scenario_sizes = [10]
-
-## Trials per outbreak scenario size ##
-# Set the number of simulations, If only 1 store, we only simulate one outbreak
-# if chains[chain] == 1:
-#     no_of_trials_per_scenario = 1
-# else:
-#     no_of_trials_per_scenario = 5 + round(
-#         (no_of_outbreak_cases * chains[chain]) / 5
-#     )
-no_of_trials_per_scenario = 1
-
-
-def get_xy(outbreak_scenario, scenario):
+def get_xy(outbreak_scenario, scenario, df_population):
     df = pd.DataFrame({"Gitter_ID": outbreak_scenario})
-    population_data = pd.read_pickle(
-        os.path.join("Outputs", "Population", f"population_{scenario}.pkl")
-    )
+    population_data = df_population
+
     df = df.merge(
         population_data[["x_centroid", "y_centroid"]],
         on="Gitter_ID",
@@ -35,34 +18,49 @@ def get_xy(outbreak_scenario, scenario):
     return df
 
 
-# As we want to make the artificial Outbreaks reproducible, we set the seed for the generation of random numbers
-# random.seed(3)
+def generate_outbreak(
+    investigation_scenario, df_shops, df_population, list_outbreak_scenario_sizes
+):
+    ## Trials per outbreak scenario size ##
+    # Set the number of simulations, If only 1 store, we only simulate one outbreak
+    # if chains[chain] == 1:
+    #     no_of_trials_per_scenario = 1
+    # else:
+    #     no_of_trials_per_scenario = 5 + round(
+    #         (no_of_outbreak_cases * chains[chain]) / 5
+    #     )
 
+    no_of_trials_per_scenario = 1
 
-all_stores = pd.read_pickle(
-    os.path.join("Outputs", "Stores", "stores_" + str(scenario) + ".pkl")
-)
+    # As we want to make the artificial Outbreaks reproducible, we set the seed for the generation of random numbers
+    # random.seed(3)
 
-n_of_chains = all_stores["Chain"].nunique()
-# Number of stores per chain
-chains = all_stores.groupby(["Chain"])["Chain"].agg("count")
+    all_stores = df_shops
 
-for chain in chains.index:
-    for no_of_outbreak_cases in list_outbreak_scenario_sizes:
-        for trial in range(0, no_of_trials_per_scenario):
-            outbreak_name = f"{chain}_{no_of_outbreak_cases}_{trial}"
+    n_of_chains = all_stores["Chain"].nunique()
+    # Number of stores per chain
+    chains = all_stores.groupby(["Chain"])["Chain"].agg("count")
 
-            outbreak_scenario_cells = generate_outbreak(
-                chain, no_of_outbreak_cases, all_stores, scenario
-            )
+    for chain in chains.index:
+        for no_of_outbreak_cases in list_outbreak_scenario_sizes:
+            for trial in range(0, no_of_trials_per_scenario):
+                outbreak_name = f"{chain}_{no_of_outbreak_cases}_{trial}"
 
-            outbreak_scenario = get_xy(outbreak_scenario_cells, scenario)
+                outbreak_scenario_cells = generate_outbreak(
+                    chain, no_of_outbreak_cases, all_stores, investigation_scenario
+                )
 
-            output_dir = os.path.join("Outputs", "Outbreaks", f"Scenario_{scenario}")
-            os.makedirs(
-                output_dir,
-                exist_ok=True,
-            )
-            outbreak_scenario.to_pickle(
-                os.path.join(output_dir, f"Outbreak_{outbreak_name}.pkl")
-            )
+                outbreak_scenario = get_xy(
+                    outbreak_scenario_cells, investigation_scenario, df_population
+                )
+
+                output_dir = os.path.join(
+                    "Outputs", "Outbreaks", f"Scenario_{investigation_scenario}"
+                )
+                os.makedirs(
+                    output_dir,
+                    exist_ok=True,
+                )
+                outbreak_scenario.to_pickle(
+                    os.path.join(output_dir, f"Outbreak_{outbreak_name}.pkl")
+                )
