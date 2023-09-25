e9ffa6a133423c1b58f1c31e81ae45d577fc2f3e
Sandra Rudeloff <srudeloff@outlook.com>
2023-09-25 10:05:14 +0200

    traceback initialization

---
+++ Traceback_Model/traceback_init.R
@@ -0,0 +1,27 @@
+source("trace_outbreak.R")
+
+investigation_scenarios <- c(1)
+no_of_cells <- 100
+delta <- 0.05  # Define half the side length of a square
+set.seed(123)
+
+results_df <- data.frame(
+  scenario_id = character(),
+  outbreak_id = character(),
+  chain_id = character(),
+  GLRT_statistic = numeric(),
+  p_value = numeric(),
+  decision = character(),
+  alpha = numeric(),
+  beta = numeric(),
+  likelihood_value = numeric(),
+  stringsAsFactors = FALSE #tells R not to convert character vectors to factors when creating a data frame
+)
+
+for (scenario in investigation_scenarios) {
+  print(scenario)
+  current_results <- analyze_scenario(scenario, no_of_cells, delta, results_df)
+  results_df <- rbind(results_df, current_results)
+}
+
+write.csv(results_df, "Data/results.csv", row.names = FALSE)
